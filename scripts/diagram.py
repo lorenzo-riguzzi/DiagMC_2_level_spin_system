687e7c18427a1fdeefd6ecfabd76c7a9cd2ec847
Lorenzo Riguzzi <lorenzo.riguzzi@studio.unibo.it>
2026-06-06 15:57:42 +0200

    Created the class Diagram

---
+++ scripts/diagram.py
@@ -0,0 +1,18 @@
+class Diagram():
+    
+    """ This class implements Feynman diagrams. 
+    
+        Attributes:
+            beta: inverse temperature (gives the length of the diagram)
+            s_0: spin of the initial spin of the first segment of the diagram (can be either +/-1, the default value is set arbitrary to -1)
+            vertices: list of vertices in the diagram (where the spin flips occur, the default value is an empty list)
+            Gamma: external field along the x axis
+            h: external field along the z axis
+    """
+    
+    def __init__(self, beta: float, s_0: int = -1, vertices: list = None, Gamma: float = 0, h: float = 0):
+        self.beta = beta
+        self.s_0 = s_0
+        self.vertices = [] if vertices is None else vertices
+        self.Gamma = Gamma
+        self.h = h
