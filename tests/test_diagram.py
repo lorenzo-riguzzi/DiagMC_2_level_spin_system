5597d83bb33c5d7592df6c46cca5fc612aba65fc
Lorenzo Riguzzi <lorenzo.riguzzi@studio.unibo.it>
2026-06-17 23:13:39 +0200

    Created a tests folder and started working on test_simulation.py

--- tests/test_diagram.py
+++ tests/test_diagram.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from scripts.diagram import Diagram, Diagram_Random
 
---
+++ tests/test_simulation.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from scripts.simulation import single_run
+
+""" Tests for the methods in the simulation.py file """
+
+
+
