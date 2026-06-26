9e3d6ac1aa9d1b2f6191096da7b4f280b657a27c
Lorenzo Riguzzi <lorenzo.riguzzi@studio.unibo.it>
2026-06-26 12:01:43 +0200

    Finished working on the plot_sweep function

--- plots/plot_sweep.py
+++ plots/plot_sweep.py
@@ -3,4 +3,71 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def plot_sweep(results_file: str = "sweep_results.csv"):
-    pass
+    """
+        Plots the results of the sweep simulation
+        
+        PARAMETERS:
+        results_file = name of the file with the required results (the file must be in the results directory)
+    """
+    
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    project_root = os.path.dirname(script_dir)
+    
+    data_dir = os.path.join(project_root, "results")
+    
+    file_path = os.path.join(data_dir, results_file)
+    
+    if not os.path.exists(file_path):
+        print(f"Error: Results file '{file_path}' not found.")
+        return
+    
+    dataframe = pd.read_csv(file_path)
+    
+    variable_name = dataframe.columns[0]
+    
+    fixed_var_1_name = dataframe.columns[5]
+    fixed_var_1_value = dataframe[fixed_var_1_name].iloc[1]
+    
+    fixed_var_2_name = dataframe.columns[6]
+    fixed_var_2_value = dataframe[fixed_var_2_name].iloc[1]
+    
+    fig_m_z, plt_mz = plt.subplots(figsize=(8, 5))
+    
+    plt_mz.scatter(dataframe[variable_name], dataframe["m_z (MC)"], label = "MC", color = "blue", s = 20)
+    plt_mz.plot(dataframe[variable_name], dataframe["m_z (Analytical)"], label = "Analytical", color = "blue")
+    
+    plt_mz.grid(True, which="both", linestyle="--", alpha=0.5)
+    
+    plt_mz.set_title(f"Sweep results over ${variable_name}$ of $m_z$ for $\{fixed_var_1_name}={fixed_var_1_value}$ and $\{fixed_var_2_name}={fixed_var_2_value}$")
+    plt_mz.set_ylabel("$m_z$")
+    plt_mz.set_xlabel(f"${variable_name}$")
+    
+    plt_mz.legend()
+    
+    output_name_m_z = os.path.splitext(results_file)[0] + "_m_z"
+    output_path_m_z = os.path.join(project_root, "results", f"{output_name_m_z}.png")
+    fig_m_z.savefig(output_path_m_z, dpi=300)
+    
+    fig_m_x, plt_mx = plt.subplots(figsize=(8, 5))
+    
+    plt_mx.scatter(dataframe[variable_name], dataframe["m_x (MC)"], label = "MC", color = "blue", s = 20)
+    plt_mx.plot(dataframe[variable_name], dataframe["m_x (Analytical)"], label = "Analytical", color = "blue")
+    
+    plt_mx.grid(True, which="both", linestyle="--", alpha=0.5)
+    
+    plt_mx.set_title(f"Sweep results over ${variable_name}$ of $m_x$ for $\{fixed_var_1_name}={fixed_var_1_value}$ and $\{fixed_var_2_name}={fixed_var_2_value}$")
+    plt_mx.set_ylabel("$m_x$")
+    plt_mx.set_xlabel(f"${variable_name}$")
+    
+    plt_mx.legend()
+    
+    output_name_m_x = os.path.splitext(results_file)[0] + "_m_x"
+    output_path_m_x = os.path.join(project_root, "results", f"{output_name_m_x}.png")
+    fig_m_x.savefig(output_path_m_x, dpi=300)
+    
+    plt.show()
+    
+if __name__ == "__main__":
+    plot_sweep()
+
